825b3cda349eb4fa6723346e92ec2ee9febc1327
Muhammad Naufal Fawwaz Ramadhan <103298559+Luchaz@users.noreply.github.com>
2024-12-15 19:42:52 +0700

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,31 +1,30 @@
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Random import get_random_bytes
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-# Generate RSA Keys (Public/Private)
-def generate_rsa_keys():
-    key = RSA.generate(2048)
-    private_key = key.export_key()
-    public_key = key.publickey().export_key()
+def generate_primes():
+    # Hardcoded small primes for simplicity
+    return [3, 5, 7, 11, 13]
 
-    # Save keys to files
-    with open("private.pem", "wb") as priv_file:
-        priv_file.write(private_key)
-    with open("public.pem", "wb") as pub_file:
-        pub_file.write(public_key)
-    
-    return private_key, public_key
+def rsa_keygen():
+    primes = generate_primes()
+    p = primes[0]
+    q = primes[1]
+    n = p * q
+    totient = (p - 1) * (q - 1)
 
-# Encrypt DES key using RSA public key
-def encrypt_with_rsa(public_key, des_key):
-    public_key = RSA.import_key(public_key)
-    cipher = PKCS1_OAEP.new(public_key)
-    encrypted_key = cipher.encrypt(des_key)
-    return encrypted_key
+    e = 3  # Start with a small odd number
+    while gcd(e, totient) != 1:
+        e += 2  # Increment by 2 to ensure odd numbers only
 
-# Decrypt DES key using RSA private key
-def decrypt_with_rsa(private_key, encrypted_key):
-    private_key = RSA.import_key(private_key)
-    cipher = PKCS1_OAEP.new(private_key)
-    des_key = cipher.decrypt(encrypted_key)
-    return des_key
+    d = pow(e, -1, totient)  # Compute modular inverse
+    return (e, n), (d, n)  # Return public and private keys
+
+def rsa_encrypt(message, key):
+    e, n = key
+    return [pow(ord(char), e, n) for char in message]
+
+def rsa_decrypt(ciphertext, key):
+    d, n = key
+    return ''.join([chr(pow(char, d, n)) for char in ciphertext])
